190f96d58b0363450fab7c02e11f6fc42b21b467
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2025-01-13 13:50:02 +0000

    Added assert views trait

---
+++ src/AssertsViews.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AnthonyEdmonds\LaravelTestingTraits;
+
+use Illuminate\Contracts\View\View;
+
+trait AssertsViews
+{
+    use SetsViewVariables;
+
+    public function assertViewRenders(
+        View $view,
+        array $attributes,
+        array $errors = [],
+        array $old = [],
+        array $slots = [],
+    ): void {
+        $this->setViewAttributes($attributes);
+        $this->setViewErrors($errors);
+        $this->setRequestOld($old);
+
+        foreach ($slots as $slot) {
+            $this->setViewSlot(
+                $slot['name'] ?? '',
+                    $slot['html'] ?? '',
+                    $slot['data'] ?? [],
+            );
+        }
+
+        $this->assertIsString(
+            $view->render(),
+            $view->name() . ' failed to render',
+        );
+    }
+}
